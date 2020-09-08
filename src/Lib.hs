2cf19e8bb219f7968612fba3ed883edb7f675292
Gleb Akhmerov <g.akhmerov@gmail.com>
2020-09-08 21:23:03 +0400

    Code to execute sql script

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,14 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE OverloadedStrings #-}
+
+module Lib where
+
+import Data.String ( fromString )
+import Database.PostgreSQL.Simple ( connectPostgreSQL, execute_, begin, rollback )
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+  migrationSql <- readFile "migrations/1.sql"
+  conn <- connectPostgreSQL "host='localhost' port='5432' dbname='haskell-news-server' user='postgres'"
+  begin conn
+  print =<< execute_ conn (fromString migrationSql)
+  rollback conn
