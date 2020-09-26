612bf4abe60c0512b316a91df51576ddb3e74dd7
Gleb Akhmerov <g.akhmerov@gmail.com>
2020-09-26 09:29:35 +0400

    Move post select-query filtering to Queries.Post

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,16 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE QuasiQuotes #-}
-{-# LANGUAGE RecordWildCards #-}
 
 module Lib where
 
-import Data.Int (Int32)
-import Data.Set (Set)
+
 import Data.String (fromString)
-import Data.Text (Text)
-import Data.Time (LocalTime)
-import Data.Vector (Vector)
-import qualified Data.Vector as Vector (fromList)
 
 import Database.PostgreSQL.Simple (execute_, begin, rollback)
 import Database.Beam hiding (date)
@@ -23,93 +16,7 @@ import Queries.Draft
 import Queries.Post
 import Queries.Tag
 import Queries.User
-import Queries.Util
 
-data PostFilter
-  = PfPublishedAt LocalTime
-  | PfPublishedAtLt LocalTime
-  | PfPublishedAtGt LocalTime
-  | PfAuthorName Text Text
-  | PfCategoryId Int32
-  | PfTagId Int32
-  | PfTagIdsIn [Int32]
-  | PfTagIdsAll [Int32]
-  | PfNameSubstring Text
-  | PfContentSubstring Text
-  deriving (Eq, Ord)
-
-applyOneFilterToQuery
-  :: PostFilter
-  -> DbWith
-       (DbQ s
-         ( PostT (DbQExpr s)
-         , UserT (DbQExpr s)
-         , T2 s (Vector Int32) (Vector Text)
-         , T2 s (Vector Int32) (Vector Text)
-         , DbQExpr s (Vector Int32)))
-  -> DbWith
-       (DbQ s
-         ( PostT (DbQExpr s)
-         , UserT (DbQExpr s)
-         , T2 s (Vector Int32) (Vector Text)
-         , T2 s (Vector Int32) (Vector Text)
-         , DbQExpr s (Vector Int32)))
-applyOneFilterToQuery flt withQuery = do
-  postQuery <- withQuery
-  pure $ do
-    row@(post, user, (_catIds, _catNames), (tagIds, _tagNames), _additionalPhotoIds) <- postQuery
-    guard_ $
-      case flt of
-        PfPublishedAt date ->
-          postPublishedAt post ==. val_ date
-        PfPublishedAtLt date ->
-          postPublishedAt post <. val_ date
-        PfPublishedAtGt date ->
-          postPublishedAt post >. val_ date
-        PfAuthorName firstName lastName ->
-          userFirstName user ==. val_ firstName &&. userLastName user ==. val_ lastName
-        PfCategoryId cId ->
-          postCategoryId post ==. val_ cId
-        PfTagId tId ->
-          val_ (Vector.fromList [tId]) `isSubsetOf_` tagIds
-        PfTagIdsIn tIds ->
-          val_ (Vector.fromList tIds) `isSubsetOf_` tagIds
-        PfTagIdsAll tIds ->
-          val_ (Vector.fromList tIds) `isSupersetOf_` tagIds
-        _ ->
-          val_ True
-    pure row
-
-applyFiltersToQuery
-  :: Set PostFilter
-  -> DbWith
-       (DbQ s
-         ( PostT (DbQExpr s)
-         , UserT (DbQExpr s)
-         , T2 s (Vector Int32) (Vector Text)
-         , T2 s (Vector Int32) (Vector Text)
-         , DbQExpr s (Vector Int32)))
-  -> DbWith
-       (DbQ s
-         ( PostT (DbQExpr s)
-         , UserT (DbQExpr s)
-         , T2 s (Vector Int32) (Vector Text)
-         , T2 s (Vector Int32) (Vector Text)
-         , DbQExpr s (Vector Int32)))
-applyFiltersToQuery filters query =
-  foldr applyOneFilterToQuery query filters
-
-data Order
-  = Ascending
-  | Descending
-
-data PostOrder = PostOrder Order PostOrderBy
-
-data PostOrderBy
-  = PoPublishedAt
-  | PoAuthorName
-  | PoCategoryName
-  | PoPhotoCount
 
 someFunc :: IO ()
 someFunc = do
--- src/Queries/Post.hs
+++ src/Queries/Post.hs
@@ -1,12 +1,18 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeFamilies #-}
+
 module Queries.Post where
 
 
 import Data.Function ((&))
 import Data.Int (Int32)
+import Data.Set (Set)
 import Data.Text (Text)
+import Data.Time (LocalTime)
 import Data.Vector (Vector)
+import qualified Data.Vector as Vector (fromList)
 
-import Database.Beam
+import Database.Beam hiding (date)
 import Database.Beam.Postgres
 
 import BeamSchema
@@ -39,3 +45,68 @@ postsWithCategories = do
                     , (pgArrayAgg (categoryId cTree), pgArrayAgg (categoryName cTree))
                     , (pgArrayAgg (tagId tag), pgArrayAgg (tagName tag))
                     , pgArrayAgg (postAdditionalPhotoPhotoId postAdditionalPhoto)))
+
+
+data PostFilter
+  = PfPublishedAt LocalTime
+  | PfPublishedAtLt LocalTime
+  | PfPublishedAtGt LocalTime
+  | PfAuthorName Text Text
+  | PfCategoryId Int32
+  | PfTagId Int32
+  | PfTagIdsIn [Int32]
+  | PfTagIdsAll [Int32]
+  | PfNameSubstring Text
+  | PfContentSubstring Text
+  deriving (Eq, Ord)
+
+
+applyFiltersToQuery
+  :: Set PostFilter
+  -> DbWith
+       (DbQ s
+         ( PostT (DbQExpr s)
+         , UserT (DbQExpr s)
+         , T2 s (Vector Int32) (Vector Text)
+         , T2 s (Vector Int32) (Vector Text)
+         , DbQExpr s (Vector Int32)))
+applyFiltersToQuery filters =
+  foldr applyFilter postsWithCategories filters
+  where
+    applyFilter flt withQuery = do
+      postQuery <- withQuery
+      pure $ do
+        row@(post, user, (_catIds, _catNames), (tagIds, _tagNames), _additionalPhotoIds) <- postQuery
+        guard_ $
+          case flt of
+            PfPublishedAt date ->
+              postPublishedAt post ==. val_ date
+            PfPublishedAtLt date ->
+              postPublishedAt post <. val_ date
+            PfPublishedAtGt date ->
+              postPublishedAt post >. val_ date
+            PfAuthorName firstName lastName ->
+              userFirstName user ==. val_ firstName &&. userLastName user ==. val_ lastName
+            PfCategoryId cId ->
+              postCategoryId post ==. val_ cId
+            PfTagId tId ->
+              val_ (Vector.fromList [tId]) `isSubsetOf_` tagIds
+            PfTagIdsIn tIds ->
+              val_ (Vector.fromList tIds) `isSubsetOf_` tagIds
+            PfTagIdsAll tIds ->
+              val_ (Vector.fromList tIds) `isSupersetOf_` tagIds
+            _ ->
+              val_ True
+        pure row
+
+data Order
+  = Ascending
+  | Descending
+
+data PostOrder = PostOrder Order PostOrderBy
+
+data PostOrderBy
+  = PoPublishedAt
+  | PoAuthorName
+  | PoCategoryName
+  | PoPhotoCount
