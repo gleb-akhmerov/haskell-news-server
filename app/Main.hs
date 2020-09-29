a3a31e139689b389f0f4bf6ea2242359c3f7845d
Gleb Akhmerov <g.akhmerov@gmail.com>
2020-09-29 16:42:08 +0400

    Begin implementing WAI routing

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,67 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Lib
+
+import Data.Bits (xor)
+import Data.ByteString (ByteString)
+import Data.ByteString.Builder (byteString)
+import Data.Int (Int32)
+import Data.Text (Text)
+
+import Data.Aeson (encode, ToJSON)
+import Database.Beam.Postgres
+import Network.Wai
+import Network.Wai.Handler.Warp (run)
+import Network.HTTP.Types (status200, status404, hContentType, Status, ResponseHeaders)
+
+import Queries.Author
+import Queries.Photo
+
+
+matchRoute :: ByteString -> [Text] -> ByteString -> [Text] -> Bool
+matchRoute method path rMethod rPath =
+  let match (p, rp) =
+        (p == "*") `xor` (p == rp)
+  in method == rMethod
+     && length path == length rPath
+     && all match (zip path rPath)
+
+responseBS :: Status -> ResponseHeaders -> ByteString -> Response
+responseBS s h = responseBuilder s h . byteString
+
+notFound :: Response
+notFound = responseBuilder status404 [] "Not found"
+
+appGetPhoto :: (Pg (Maybe ByteString) -> IO (Maybe ByteString)) -> Int32 -> IO Response
+appGetPhoto runPg pId = do
+  mPhotoBytes <- runPg $ getPhoto pId
+  pure $ case mPhotoBytes of
+    Nothing ->
+      notFound
+    Just photoBytes ->
+      responseBS status200 [] photoBytes
+
+appGetEntity :: ToJSON a => (Int32 -> Pg (Maybe a)) -> Int32 -> Pg Response
+appGetEntity getEntity eId = do
+  mEntity <- getEntity eId
+  pure $ case mEntity of
+    Nothing ->
+      notFound
+    Just entity ->
+      responseLBS
+        status200
+        [(hContentType, "application/json")]
+        (encode entity)
 
 main :: IO ()
-main = someFunc
+main = do
+  conn <- connectPostgreSQL "host='localhost' port='5432' dbname='haskell-news-server' user='postgres'"
+  run 3000 $ \req send ->
+    let method = requestMethod req
+        path = pathInfo req
+    in do response <- runBeamPostgres conn $
+            if matchRoute "GET" ["author", "*"] method path
+            then appGetEntity getAuthor 1
+            else pure notFound
+          send response
---
+++ src/Queries/Photo.hs
@@ -0,0 +1,30 @@
+module Queries.Photo where
+
+
+import Data.ByteString (ByteString)
+import Data.Int (Int32)
+
+import Database.Beam
+import Database.Beam.Backend.SQL.BeamExtensions
+import Database.Beam.Postgres
+
+import BeamSchema
+
+
+createPhoto :: ByteString -> Pg Int32
+createPhoto photoBytes = do
+  [photo] <- runInsertReturningList $ insert (dbPhoto newsDb) $
+    insertExpressions
+      [ Photo { photoId      = default_
+              , photoContent = val_ photoBytes
+              }
+      ]
+  pure (photoId photo)
+
+
+getPhoto :: Int32 -> Pg (Maybe ByteString)
+getPhoto gPhotoId = do
+  mPhoto <- runSelectReturningOne $ select $
+              filter_ (\p -> photoId p ==. val_ gPhotoId)
+                      (all_ (dbPhoto newsDb))
+  pure (fmap photoContent mPhoto)
